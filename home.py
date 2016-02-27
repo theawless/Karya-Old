bb784d7216ea2099694ef3645699f56354a808e9
abhinav.prince2012 <abhinav.prince2012@gmail.com>
2016-02-27 13:11:13 +0530

    Added functions to open applications/files

--- home.py
+++ home.py
@@ -1,9 +1,9 @@
-import os
+import os, sys, subprocess
 import gi
 import urllib.parse
 import urllib.request
 import json
-
+#from gui_functions import search, local_search, exec_task
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, WebKit
 
@@ -33,6 +33,10 @@ class EntryWindow(Gtk.Window):
         button02.connect("clicked", self.search_local, vbox, Entry01)
         vbox.pack_start(button02, True, True, 0)
 
+        button03 = Gtk.Button.new_with_label("Execute task")
+        button03.connect("clicked", self.exec_task, vbox, Entry01)
+        vbox.pack_start(button03, True, True, 0)
+
     def search(self, button, url, Entry01, vbox):
         print("\"Search\" button was clicked")
         search_keywords = Entry01.get_text()
@@ -55,6 +59,37 @@ class EntryWindow(Gtk.Window):
                     print(file)
 #        button03 = Gtk.Button.new_with_label("test successfull")
 #        vbox.pack_start(button03, True, True, 0)
+    
+    def exec_task(self, button, vbox, Entry01):
+        command = Entry01.get_text()
+#Shutdown now
+        if command=="shutdown now":
+            os.system("sudo shutdown 0")
+#Reboot
+        elif command==("reboot" or "restart"):
+            os.system("sudo shutdown 2 -r")
+#shutdown in x minutes
+        elif command[0]=='s'and command[1]=='h'and command[2]=='u'and command[3]=='t'and command[4]=='d'and command[5]=='o'and command[6]=='w'and command[7]=='n':
+            os.system("sudo shutdown "+command[12])
+#Open Applications or files
+        elif  command[0]==('o' or 'O') and command[1]=='p' and command[2]=='e' and command[3]=='n':
+            #print(command[5:])
+            subprocess.call(["xdg-open", command[5:]])
+            os.system(command[5:])
+            
+#else
+        else:
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.WARNING,Gtk.ButtonsType.OK_CANCEL, "Command Not Found")
+            dialog.format_secondary_text("Enter a valid command")
+            response = dialog.run()
+            if response == (Gtk.ResponseType.OK or Gtk.ResponseType.CANCEL):
+                dialog.destroy()
+                
+
+
+
+
+
 
 win = EntryWindow()
 win.connect("delete-event", Gtk.main_quit)
