f5ccc04854f4a4e5350b342da03b85a0baf3bf37
abhinav.prince2012 <abhinav.prince2012@gmail.com>
2016-04-23 03:17:53 +0530

    Integrated Sphinx voice recog with Sarcina

--- Sarcina/homepage/homepage.py
+++ Sarcina/homepage/homepage.py
@@ -1,5 +1,10 @@
-import gi
+import threading
 
+import gi
+import time
+
+from homepage.recogspeechbg import SpeechRecogniser
+from homepage.setlog import logger
 from .search import Search
 
 gi.require_version('Gtk', '3.0')
@@ -10,6 +15,7 @@ class HomepageHandler:
     """
     This is a signal handler class for homepage
     """
+
     def __init__(self):
         """
         constructor of HomepageHandler
@@ -59,13 +65,30 @@ class HomePage:
     """
     This class represent a class for Homepage of Sarcina
     """
+
     def __init__(self):
         """
         Constructor of HomePage
         :return: NULL
         """
         self.builder = Gtk.Builder()
+        time.sleep(1)
+        self.recogniser = SpeechRecogniser(self.search_entry_bar_text_changer)
+        logger.debug("Constructed recogniser, Inside __init__ of homepage")
+        self.threader = threading.Thread(target=self.recogniser.start_recognising)
+        self.threader.daemon = True
+        self.threader.start()
         pass
+
+    def search_entry_bar_text_changer(self, text: str):
+        """
+        displays the recognized text in search entry
+        :type text: basestring
+        :param text: recognized text
+        :return: Null
+        """
+        search_entry = self.builder.get_object("search_entry")
+        search_entry.set_text(text)
 
     def get_homepage_box(self):
         """
